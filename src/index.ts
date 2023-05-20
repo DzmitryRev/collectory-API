225960bcce6bfcac5330b0659f0c9a5b708e2a90
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-05-20 20:40:10 +0300

    Install and set up eslint
    - Install: eslint and required packages;
    - Add .eslintrc.json and set up rules;

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import app from "./server";
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log("Server is running");
+	console.log("Server is running");
 });
